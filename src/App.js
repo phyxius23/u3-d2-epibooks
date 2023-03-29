bc485d21ebf359178a119598c590273c3bb7f38a
phyxius23 <30ntin@gmail.com>
2023-03-29 16:55:46 +0200

    add props to WelcomeComponent

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <MyNav brand="EpiBooks"/>
-      <Welcome />
+      <Welcome title="EpiBooks"/>
       <AllTheBooks />
       <MyFooter brand="EpiBooks" />
     </>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,14 +1,12 @@
 import {Container} from 'react-bootstrap';
 
-const Welcome = () => (
+const Welcome = (props) => (
   <Container className='my-5 p-5 bg-light rounded'>
-  {/* <div className="mt-5 p-5 bg-light rounded container"> */}
-    <h1 className="display-4">Hello, world!</h1>
+    <h1 className="display-4">{props.title}</h1>
     <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
     <p className="lead">
       <a className="btn btn-dark btn-lg" href='#learn' role="button">Learn more</a>
     </p>
-  {/* </div> */}
   </Container>
 );
 
